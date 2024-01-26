1fe1a687e9f4710132407fc59c9a6f4acef37d60
Joshua Breckeen <86539171+AltaArts@users.noreply.github.com>
2024-01-26 07:10:44 -0500

    up version

--- FileIconsMenu/Scripts/Prism_FileIconsMenu_Variables.py
+++ FileIconsMenu/Scripts/Prism_FileIconsMenu_Variables.py
@@ -50,7 +50,7 @@ import os
 
 class Prism_FileIconsMenu_Variables(object):
     def __init__(self, core, plugin):
-        self.version = "v2.0.2-1.4"
+        self.version = "v2.0.2-1.5"
         self.pluginName = "FileIconsMenu"
         self.pluginType = "Custom"
         self.platforms = ["Windows", "Linux", "Darwin"]
