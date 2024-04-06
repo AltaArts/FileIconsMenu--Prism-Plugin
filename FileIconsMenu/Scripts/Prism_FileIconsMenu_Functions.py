b513f5f6313daa186042d8f393d0ef7d1121a5d2
Joshua Breckeen <86539171+AltaArts@users.noreply.github.com>
2024-04-06 08:27:21 -0400

    Updated for Prims 2.0.5

--- FileIconsMenu/Scripts/Prism_FileIconsMenu_Functions.py
+++ FileIconsMenu/Scripts/Prism_FileIconsMenu_Functions.py
@@ -45,15 +45,15 @@
 ####################################################
 
 
-try:
-    from PySide2.QtCore import *
-    from PySide2.QtGui import *
-    from PySide2.QtWidgets import *
-except:
-    from PySide.QtCore import *
-    from PySide.QtGui import *
+import os
+import shutil
+import json
+import re
 
-import os, shutil, json, re
+from qtpy.QtCore import *
+from qtpy.QtGui import *
+from qtpy.QtWidgets import *
+
 
 from PrismUtils.Decorators import err_catcher_plugin as err_catcher
 
